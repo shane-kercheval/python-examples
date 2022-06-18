83416752d7730f5c4bb77e1a81e7a6abdfffd8b8
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2022-06-18 09:55:18 -0700

    rename files

--- examples/lightgbm_example.py
+++ examples/lightgbm_example.py
@@ -1,10 +1,11 @@
 import sys
 import lightgbm as lgb
 
+
 def main():
     print(sys.version)
     print(sys.argv)
-    model = lgb.LGBMClassifier(learning_rate=0.09,max_depth=-5,random_state=42)
+    model = lgb.LGBMClassifier(learning_rate=0.09, max_depth=-5, random_state=42)
     print(model)
 
 
